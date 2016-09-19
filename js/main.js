bc4a015da9a7cbe6eab8e82e349383554fc15782
Sthefany <Sthefany>
2016-09-19 10:57:58 -0500

    Eliminando comentarios

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 (function(){
-    // Variables
     var lista = document.getElementById("lista"),
         tareaInput = document.getElementById("tareaInput"),
         btnNuevaTarea = document.getElementById("btn-agregar");
-    // Funciones
     var agregarTarea = function (){
         var tarea = tareaInput.value,
             nuevaTarea = document.createElement("li"),
@@ -14,15 +12,10 @@
             tareaInput.className = "error";
             return false;
         }
-        // Agregamos contenido al enlace
         enlace.appendChild(contenido);
-        // Le establecemos un atributo href
         enlace.setAttribute("href", "#");
-        // Agregamos el enlace (a) a la nueva tarea (li)
         nuevaTarea.appendChild(enlace);
-        //Agregamos la nueva tarea a la lista
         lista.appendChild(nuevaTarea);
-        // Limpiar :)
         tareaInput.value = "";
         
         for (var i = 0; i <= lista.children.length -1; i++){
@@ -41,15 +34,11 @@
     var eliminarTarea = function(){
          this.parentNode.removeChild(child);
     };
-    
-    // Eventos
-    
-     // Agregar Tarea
+
+
     btnNuevaTarea.addEventListener("click",agregarTarea);
-    // Comprobar Input
     tareaInput.addEventListener("click",comprobarInput);
-    
-    // Borrando Elementos de la lista
+
     for (var i = 0; i <= lista.children.length -1; i++){
         
         lista.children[i].addEventListener("click",eliminarTarea);
